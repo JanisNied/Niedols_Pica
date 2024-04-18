b3b37fa1e2ee04fa28af80b60168af5203cb093c
Janis <122255186+JanisNied@users.noreply.github.com>
2024-04-18 09:52:15 +0300

    Direktoriju organizācija un .java failu pievienošana

---
+++ src/main/Main.java
@@ -0,0 +1,20 @@
+package main;
+
+import java.awt.EventQueue;
+import states.LoginWindow;
+
+public class Main {
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					LoginWindow frame = new LoginWindow();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+}
