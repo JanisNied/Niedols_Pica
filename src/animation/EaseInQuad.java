5bedbaaf0a22a60f2fc35170ed28970c761b5463
Janis <122255186+JanisNied@users.noreply.github.com>
2024-04-23 15:52:23 +0300

    Decided to redo interpolation

--- src/animation/EaseInQuad.java
+++ src/animation/EaseInQuad.java
@@ -2,9 +2,9 @@ package animation;
 
 import org.jdesktop.animation.timing.interpolation.Interpolator;
 
-public class CustomInterpolator implements Interpolator {
-    @Override
+public class EaseInQuad implements Interpolator {
+	@Override
     public float interpolate(float x) {
-    	return 1 - (1 - x) * (1 - x);
+    	return x*x;
     }
 }
